497a3ed5ba41c1acab2bd7cafdfe40f64c4c2f37
lukeponga-dev <lukegilchrist69@hotmail.com>
2022-04-17 01:20:16 +1200

    Updated Readme

--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@ public class Main {
         public static void main(String[] args){
             Stack stack = new Stack();
 
-            stack.push("S");
-            stack.push("T");  
-            stack.push("A");
-            stack.push("C");
-            stack.push("K");
+            // stack.push("S");
+            // stack.push("T");  
+            // stack.push("A");
+            // stack.push("C");
+            // stack.push("K");
             stack.print();
            stack.pop();
             stack.print();
