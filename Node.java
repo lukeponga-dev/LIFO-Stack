cdc68552face249189deafd961b9c183c0489ef5
lukeponga-dev <lukegilchrist69@hotmail.com>
2022-04-16 20:04:33 +1200

    2

---
+++ Node.java
@@ -0,0 +1,11 @@
+public class Node {
+   public String value;
+   public Node next;
+
+    public Node (String x){
+        value = x;
+        next = null;
+    }
+    
+    
+}
--- Stack.java
+++ Stack.java
@@ -1,4 +1,8 @@
 public class Stack{
+    // Class Variables
+        private Node head;
+
+    // Public Methods
 
     /**
      * 1. Stack: define a class called Stack in a file called Stack.java. This class is to
@@ -11,4 +15,12 @@ public class Stack{
      * ● length(): returns as an int the number of commands in the queue.
      * ● dump(): prints the contents of the stack to standard output.
     */
+/* this method will add new node to the front of the list  */
+    public void push(String x){
+        Node newNode = new Node(x);
+        newNode.value = x;
+        newNode.next = head;
+        head = newNode;
+        return;
+    }
 }
