8af097b8797dc6e54c58c6293fb2fff5b992aa6d
lukeponga-dev <lukegilchrist69@hotmail.com>
2022-04-16 23:53:13 +1200

    push

--- Main.java
+++ Main.java
@@ -2,5 +2,17 @@ public class Main {
         public static void main(String[] args){
             Stack stack = new Stack();
 
+            stack.push("H");
+            stack.push("E");  
+                      stack.push("L");
+            stack.push("L");
+            stack.push("O");
+
+
+
+
+            stack.print();
+
+
         }
 }
--- Stack.java
+++ Stack.java
@@ -1,16 +1,40 @@
-public class Stack{
+
+public class Stack {
+   
     // Class Variables
-        private Node head;
+    private Node head;
+
+    public Stack() {
+        head = null;
+    }
 
     // Public Methods
+    /* checks if stack is empty */
+    public boolean isEmpty() {
+        if (head == null) {
+            return true;
+        }
+        return false;
+    }
 
-   
-/* this method will add new node to the front of the list  */
-    public void push(String x){
+    /* Adds new node to the front of the list */
+    public void push(String x) {
         Node newNode = new Node(x);
-        newNode.value = x;
-        newNode.next = head;
-        head = newNode;
-        return;
+
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void print() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.value + "-> ");
+            current = current.next;
+        }
+        System.out.println();
     }
 }
